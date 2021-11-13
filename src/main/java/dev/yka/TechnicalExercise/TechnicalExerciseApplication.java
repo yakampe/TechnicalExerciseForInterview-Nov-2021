8b47882684dea99728131da068f2f4debef0a426
equalcoding <info@equalcoding.dev>
2021-11-13 16:49:50 +0000

    Initial

---
+++ src/main/java/dev/yka/TechnicalExercise/TechnicalExerciseApplication.java
@@ -0,0 +1,13 @@
+package dev.yka.TechnicalExercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/yka/TechnicalExercise/TechnicalExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yka.TechnicalExercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
