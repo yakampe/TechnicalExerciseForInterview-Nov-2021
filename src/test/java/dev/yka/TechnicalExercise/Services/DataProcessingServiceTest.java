ad3843d5e282ad55953964c64fd40f5fa1c9c002
yakampe <yanis.kampe.cv@gmail.com>
2021-11-13 19:30:47 +0000

    RED: assigning resident of address

--- src/test/java/dev/yka/TechnicalExercise/Services/DataProcessingServiceTest.java
+++ src/test/java/dev/yka/TechnicalExercise/Services/DataProcessingServiceTest.java
@@ -1,9 +1,11 @@
 package dev.yka.TechnicalExercise.Services;
 
 import dev.yka.TechnicalExercise.Models.DataEntry;
+import dev.yka.TechnicalExercise.Models.Resident;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.apachecommons.ReflectionEquals;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDate;
@@ -18,16 +20,23 @@ class DataProcessingServiceTest {
     DataProcessingService dataProcessingService;
 
     List<DataEntry> listOfDataEntries;
+    Resident resident1;
 
     @BeforeEach
     public void dataSetup() {
         listOfDataEntries = new ArrayList<>();
-        listOfDataEntries.add(new DataEntry("Fred","Smith","Customs House","1 Long Street","Glasgow","Glasgow","G10","UK","M",LocalDate.parse("2010-01-01", DateTimeFormatter.ISO_DATE)));
+        resident1 = new Resident("Fred","Smith","M",LocalDate.parse("2010-01-01", DateTimeFormatter.ISO_DATE));
+        listOfDataEntries.add(new DataEntry(resident1.getFirstName(),resident1.getSurname(),"Customs House","1 Long Street","Glasgow","Glasgow","G10","UK",resident1.getGender(),resident1.getDateOfBirth()));
     }
 
     @Test
     public void returnsProcessedDataObject() {
         Assertions.assertTrue(listOfDataEntries.size() > 0);
         Assertions.assertNotNull(dataProcessingService.processData(listOfDataEntries));
+    }
+
+    @Test
+    public void assignsResidentToAddress() {
+        Assertions.assertTrue(new ReflectionEquals(dataProcessingService.processData(listOfDataEntries).get(0).getResident()).matches(resident1));
     }
 }
